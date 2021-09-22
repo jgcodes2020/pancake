3cc5a08d5228e5619518c52aff3b0b697985d1b3
jgcodes2020 <jackyguo18@hotmail.com>
2021-09-22 12:25:48 -0400

    Add lib_merger desc

--- scripts/lib_merger.py
+++ scripts/lib_merger.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+from argparse import *
 
-'''
+parser = ArgumentParser(description='''
 Merges static libraries into one, in a cross-platform manner.
+
+MSVC ABI:
 Requires at minimum VS Build Tools if not full VS.
-'''
+
+Itanium/GCC ABI:
+Requires access to ar with .mri scripts.
+''')
