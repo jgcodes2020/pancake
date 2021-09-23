80e3bdb1b60d1669ef2fca6e7d43223b434870f1
jgcodes2020 <jackyguo18@hotmail.com>
2021-09-23 12:03:21 -0400

    finish library merger script

--- scripts/lib_merger.py
+++ scripts/lib_merger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from io import StringIO
 from pathlib import Path
 from argparse import *
 import platform
 import subprocess as proc
 import sys
-
+from tempfile import TemporaryDirectory
 
 class ExistingFileAction(Action):
     def __call__(self, parser, namespace, values, opt_str=None) -> None:
@@ -42,7 +43,7 @@ MSVC ABI:
 Requires at minimum VS Build Tools if not full VS.
 
 Itanium/GCC ABI:
-Requires access to ar with .mri scripts.
+Requires access to ar.
 ''')
 parser.add_argument("-o", "--output",
                     action=NewFileAction,
@@ -72,7 +73,13 @@ def lib_exe_merge():
     vswhere_proc.check_returncode()
     vs_path = Path(vswhere_proc.stdout.strip())
 
-    lib_exe = str(vs_path.joinpath("VC\\Tools\\MSVC\\14.29.30133\\bin\\Hostx64\\x64\\lib.exe"))
+    vcver = ""
+    vcver_path = vs_path.joinpath("VC\\Auxiliary\\Build\\Microsoft.VCToolsVersion.default.txt")
+    with open(vcver_path) as vcver_f:
+        vcver = vcver_f.read()
+
+    lib_exe = str(vs_path.joinpath(
+        f"VC\\Tools\\MSVC\\{vcver}\\bin\\Hostx64\\x64\\lib.exe"))
     lib_cmd: list[str] = [lib_exe]
     try:
         lib_output = f"/OUT:{str(args.output[0])}"
@@ -86,11 +93,23 @@ def lib_exe_merge():
     )
     sys.exit(lib_proc.returncode)
 
-def ar_mri_merge():
-    pass
 
+def ar_dir_merge():
+    mri_data = None
+    with StringIO() as script:
+        print(f"create {args.output[0]}", file=script)
+        for i in args.libs:
+            print(f"addlib {i}", file=script)
+        print("save", "end", sep="\n", file=script)
+        mri_data = script.getvalue()
+    proc.run(
+        ["ar", "-M"],
+        input=mri_data.encode('utf-8')
+    )
+        
+        
 
 if platform.system() == "Windows":
     lib_exe_merge()
 elif platform.system() == "Linux":
-    ar_mri_merge()
+    ar_dir_merge()
