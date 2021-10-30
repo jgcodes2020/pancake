b9cb0d7c90e3bc0dae621691851cb6907c9239b0
jgcodes2020 <jackyguo18@hotmail.com>
2021-10-30 11:00:03 -0400

    Moved local Git repo

---
+++ src/pancake_api/include/pancake/c_api/sm64.h
@@ -0,0 +1,15 @@
+#ifndef _PANCAKE_C_SM64_H_
+#define _PANCAKE_C_SM64_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct capi_pancake_sm64;
+
+capi_pancake_sm64* pancake_make_sm64(const char* path);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
